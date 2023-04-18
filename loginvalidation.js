b0d887898948310f14aa6d3d6eb565dc98a0bb62
nbjo48970 <nbjo48970@edu.ucl.dk>
2023-04-18 09:42:38 +0200

    javascript - login

--- loginvalidation.js
+++ loginvalidation.js
@@ -1,22 +1,54 @@
-function ValidateEmail(input)
+//E-mailvalidering start - Nicoline//
+function ValidateLogin(input)
 {
 
   let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let emailError = document.getElementById("email-error");
   let formInput = document.getElementById("email-input");
 
+
+  if (inArray(input.value)) {
+    emailError.innerHTML = "PERFEKT";
+    emailError.style.color = "rgb(80, 65, 57)";
+    formInput.style.borderColor = "#e4f1f0";
+    return false;
+  }
+
   if (input.value.match(validRegex)){
-    emailError.innerHTML = "";
-    formInput.style.borderColor = "rgb(249, 247, 240)";
-    formInput.value = '';
-    document.lnform.text1.focus();
+    formInput.style.borderColor = "#e4f1f0";
+    emailError.innerHTML = "Bruger eksisterer ikke";
+    emailError.style.color = "red";
+    formInput.style.borderColor = "red";
+    document.lnform.inputem.focus();
+    // document.getElementById("button-login").setAttribute("disabled","");
     return false;
   }
     else {
-    emailError.innerHTML = "Indtast gyldig e-mailadresse";
+    emailError.innerHTML = "Ugyldig e-mailadresse";
     emailError.style.color = "red";
     formInput.style.borderColor = "red";
-    document.lnform.text1.focus();
+    document.lnform.inputem.focus();
+    // document.getElementById("button-login").setAttribute("disabled","");
     return false;
   }
 }
+let regUsers = ["peter@mail.com", "xander@mail.com", "nicoline@mail.com", "marcus@mail.com"];
+function inArray(inVal){
+    for( var i=0, len=regUsers.length; i < len; i++){
+        if (regUsers[i] == inVal) return true;
+    }
+    return false;
+}
+//E-mailvalidering slut - Nicoline//
+
+//Vis kodeord start - Nicoline//
+function HidePassword() {
+  var pw = document.getElementById("password-input");
+    if (pw.type === "password") {
+      pw.type = "text";
+    }
+    else {
+      pw.type = "password";
+    }
+}
+//Vis kodeord slut - Nicoline//
