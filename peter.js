43a758b8d58a19c428c60c9079575fcb16eb159d
PeterSogaard98 <119843293+PeterSogaard98@users.noreply.github.com>
2023-04-19 14:26:18 +0200

    peter js ifelse

--- peter.js
+++ peter.js
@@ -3,12 +3,15 @@ const filterListContainer = document.getElementById("filter-list-container")
 
 function list() {
 
-    
 
-    for (let i = 0; i < filterListArray.length; i++) {
-        let li = document.createElement('li');
-        li.innerText = filterListArray[i];
-        filterListContainer.appendChild(li);
+    if (2+2==4) {
+        for (let i = 0; i < filterListArray.length; i++) {
+            let li = document.createElement('li');
+            li.innerText = filterListArray[i];
+            filterListContainer.appendChild(li);
+        }
+    } else {
+        console.log("Math is finished and we are doomed")
     }
 }
 
