58e8c43418f0c040994f88e94c8e973e44b9e393
PeterSogaard98 <119843293+PeterSogaard98@users.noreply.github.com>
2023-04-20 09:35:44 +0200

    peter js done

--- peter.js
+++ peter.js
@@ -1,18 +1,49 @@
 let filterListArray = ["Sport", "Spil", "Gaming", "Fest", "Motion", "Studie", "Musik", "Andet"]
-const filterListContainer = document.getElementById("filter-list-container")
+let filterListContainer = document.getElementById("filter-list-container")
 
-function list() {
+    for (let i = 0; i < filterListArray.length; i++) {
+        let li = document.createElement('li');
+        li.innerText = filterListArray[i];
+        filterListContainer.appendChild(li);
 
-
-    if (2+2==4) {
-        for (let i = 0; i < filterListArray.length; i++) {
-            let li = document.createElement('li');
-            li.innerText = filterListArray[i];
-            filterListContainer.appendChild(li);
-        }
-    } else {
-        console.log("Math is finished and we are doomed")
     }
-}
 
-list()
+    function activateLiElements() {
+        let liElements = document.querySelectorAll('#filter-list-container li');
+        liElements.forEach(function (li) {
+          li.addEventListener('click', function () {
+            if (li.style.backgroundColor === 'rgb(200, 226, 223)') {
+              li.style.backgroundColor = '';
+            } else {
+              li.style.backgroundColor = 'rgb(200, 226, 223)';
+            }
+          });
+        });
+      }
+      
+      activateLiElements();
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+
+
+// function activeFilterButton() {
+
+//     let listButton = filterListContainer.getElementsByTagName('li').addEventListener("click", function())
+
+//     if (2+2==4) {
+//         listButton.innerText = "red";
+//     } else {
+        
+//     }
+
+
+// }
+
